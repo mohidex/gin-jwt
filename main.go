a24d3d0d78c7ac4f1634c51e962c1b08d8c8374f
Mohidul Islam <mohidul.islam@volue.com>
2023-03-24 23:57:36 +0600

    set up initial setup for go server

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+	router.Run(":3000")
+	fmt.Println("Server running on port 3000")
+}
